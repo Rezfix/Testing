93eea3f8957eb956725896df71f2729dc29aa7b2
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 13:53:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const client = new Discord.client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
     client.user.setGame('Rezfix -help','https://www.twitch.tv/Rezfix');
