af1c03dd691740bb7ebd906204d8bc8a27976641
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 15:48:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-const size    = config.colors;
-const rainbow = new Array(size);
-
 client.on('ready', () => {
     client.user.setGame('Rezfix -help','https://www.twitch.tv/Rezfix');
     console.log('---------------');
     console.log(' Bot Is Online')
     console.log('---------------')
   });
-
-for (var i=0; i<size; i++) {
-  var red   = sin_to_hex(i, 0 * Math.PI * 2/3); // 0   deg
-  var blue  = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-  var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
-
-  rainbow[i] = '#'+ red + green + blue;
-}
-
-function sin_to_hex(i, phase) {
-  var sin = Math.sin(Math.PI / size * 2 * i + phase);
-  var int = Math.floor(sin * 127) + 128;
-  var hex = int.toString(16);
-
-  return hex.length === 1 ? '0'+hex : hex;
-}
-
-let place = 0;
-const servers = config.servers;
-
-function changeColor() {
-  for (let index = 0; index < servers.length; ++index) {		
-    client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
-		.catch(console.error);
-		
-    if(config.logging){
-      console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
-    }
-    if(place == (size - 1)){
-      place = 0;
-    }else{
-      place++;
-    }
-  }
-}
 
 client.on('message', message => {
     var prefix = '$'; // هنا تقدر تغير البرفكس
