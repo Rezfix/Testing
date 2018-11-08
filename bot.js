587b91f35ecf95f9f03c06739a8943b41df29760
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 14:49:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,21 +81,64 @@ client.on("message", function(message) {
    }
 });
 
-clinet.on('message', ra3d => {
+clinet.on("message", async message => {
+    if(!message.channel.guild) return;
     var prefix = "$";
-                            let args = ra3d.content.split(" ").slice(1).join(" ")
-    if(ra3d.content.startsWith('#cc')) {
-        if(!args) return ra3d.channel.send('`يرجي اختيار كم لون `');
-                 if (!ra3d.member.hasPermission('MANAGE_ROLES')) return ra3d.channel.sendMessage('`**⚠ | `[MANAGE_ROLES]` لا يوجد لديك صلاحية**'); 
-                  ra3d.channel.send(`**✅ |Created __${args}__ Colors**`);
-                      setInterval(function(){})
-                        let count = 0;
-                        let ecount = 0;
-              for(let x = 1; x < `${parseInt(args)+1}`; x++){
-                ra3d.guild.createRole({name:x,
-                  color: 'RANDOM'})
-                  }
-                }
-           });
+if(message.content.startsWith(prefix + 'invites')) {
+var nul = 0
+var guild = message.guild
+await guild.fetchInvites()
+    .then(invites => {
+     invites.forEach(invite => {
+        if (invite.inviter === message.author) {
+             nul+=invite.uses
+            }
+        });
+    });
+  if (nul > 0) {
+      console.log(`\n${message.author.tag} has ${nul} invites in ${guild.name}\n`)
+      var embed = new Discord.RichEmbed()
+          .setColor("#000000")
+            .addField(`${message.author.username}`, `لقد قمت بدعوة **${nul}** شخص`)
+                  message.channel.send({ embed: embed });
+              return;
+            } else {
+               var embed = new Discord.RichEmbed()
+                .setColor("#000000")
+                .addField(`${message.author.username}`, `لم تقم بدعوة أي شخص لهذة السيرفر`)
+
+               message.channel.send({ embed: embed });
+                return;
+            }
+}
+if(message.content.startsWith(prefix + 'invite-codes')) {
+let guild = message.guild
+var codes = [""]
+message.channel.send(":postbox: **لقد قمت بأرسال جميع روابط الدعوات التي قمت بأنشائها في الخاص**")
+guild.fetchInvites()
+.then(invites => {
+invites.forEach(invite => {
+if (invite.inviter === message.author) {
+codes.push(`discord.gg/${invite.code}`)
+}
+})
+}).then(m => {
+if (codes.length < 0) {
+var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invite codes in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
+message.author.send({ embed: embed });
+return;
+} else {
+var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invite codes in ${message.guild.name}`, `Invite Codes:\n${codes.join("\n")}`)
+message.author.send({ embed: embed });
+return;
+}
+})
+}
+
+});
 
 client.login(process.env.BOT_TOKEN);
