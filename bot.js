72f9a54d61dee5be5e478e201a125fa29d3c9c8b
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-08 15:39:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -356,4 +356,15 @@ he tru to hake the server${channel.guild.name}`)
 }).catch(console.error)
 })
 
+ client.on('message', message => {
+  if(message.content.startsWith(`${prefix}invites`)) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
