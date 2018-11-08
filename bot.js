0c860c9cfdfea8d51b73f2db14f1f479fc9a71d6
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-09 01:00:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -366,56 +366,6 @@ message.channel.send(`${user} has ${inviteCount} invites.`);
 });
   }
 });
-
-var prefix = "$";
-const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-// By HeemPlayz and M7MD
-client.on("message", message => {
-    fs.writeFile('./suck.json', JSON.stringify(suck));
-});
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.forEach(g => {
-            if (suck[g.id]) {
-                if (suck[g.id].role) {
-                    var role = g.roles.get(suck[g.id].role);
-                    if (role) {
-                        role.edit({color : "RANDOM"});
-                    };
-                };
-            };
-        });
-    }, 1500);
-});
-client.on("message", message => {
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
-    var args = message.content.split(" ");
-    var command = args[0].slice(prefix.length);
-    switch(command) {
-        case "rainbow" :
-        if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("no no");
-        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
-            r.edit({color : "RANDOM"});
-            suck[message.guild.id] = {role : r.id};
-        });
-    };
-});
-
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("493237177170395155");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
- 
- 
  
 client.on("guildMemberAdd", (member) => {
     let channel = member.guild.channels.get("510096645313003520");
