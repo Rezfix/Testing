c53d9ca0876f7fd76849289e266b4cba1b382d22
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-09 14:30:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -393,4 +393,59 @@ client.on("guildMemberAdd", (member) => {
     });
 });
 
+var prefix = "!";
+const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
+// By HeemPlayz and M7MD
+client.on("message", message => {
+    fs.writeFile('./suck.json', JSON.stringify(suck));
+});
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.forEach(g => {
+            if (suck[g.id]) {
+                if (suck[g.id].role) {
+                    var role = g.roles.get(suck[g.id].role);
+                    if (role) {
+                        role.edit({color : "RANDOM"});
+                    };
+                };
+            };
+        });
+    }, 1500);
+});
+client.on("message", message => {
+    if (!message.content.startsWith(prefix)) return;
+    if (message.author.bot) return;
+    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
+    var args = message.content.split(" ");
+    var command = args[0].slice(prefix.length);
+    switch(command) {
+        case "rainbow" :
+        if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("no no");
+        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
+            r.edit({color : "RANDOM"});
+            suck[message.guild.id] = {role : r.id};
+        });
+    };
+});
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+    if(message.content.split(' ')[0] == '-ss') 
+    var embed = new Discord.RichEmbed()
+
+    .setColor('RANDOM')
+    .addField('New Suggestion',`${args}`,true)
+    .addField('By',`${message.author.tag}`,true)
+    .setTimestamp()
+
+    let suggests = message.guild.channels.find(`name`, "suggestions");
+    suggests.send(embed).then(msg => {
+        msg.react('✅')
+    .then(() => msg.react('❌'))
+    })
+
+});
+
+
 client.login(process.env.BOT_TOKEN);
