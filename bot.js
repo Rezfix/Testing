3909c2b677eede12c9a30c65f46921728901cdb2
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-10 10:40:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     client.user.setGame('Rezfix Server','https://www.twitch.tv/Rezfix');
     console.log('---------------');
-    console.log(' Bot Is Online')
+    console.log(' Rezfix Is Online')
     console.log('---------------')
   });
 
