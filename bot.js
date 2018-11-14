4533b6dc2f35ec72cdd1d237dba8b353beccde44
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-14 20:40:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -404,7 +404,7 @@ client.on('guildMemberAdd', member => {
     invites[member.guild.id] = guildInvites;
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "chat");
+    const logChannel = member.guild.channels.find(channel => channel.name === "rezfix");
     logChannel.send(`By: <@${inviter.id}>`);
   });
 });
